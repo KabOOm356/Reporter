a0a2c7afd26e7996921a2836c2505b974c2a1bb5
KabOOm 356 <kaboom356@gmail.com>
2014-07-03 21:15:11 -0500

    Changed database player name columns from 16 characters to 32 characters. Changed the data type of a few database columns to be more efficient.

---
+++ src/main/java/net/KabOOm356/Reporter/Database/MigrateToVersion9.java
@@ -0,0 +1,141 @@
+package net.KabOOm356.Reporter.Database;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import net.KabOOm356.Database.Database;
+import net.KabOOm356.Database.SQLResultSet;
+import net.KabOOm356.Reporter.Reporter;
+
+public class MigrateToVersion9
+{
+	protected static boolean migrateToVersion9(Database database)
+	{
+		boolean updated = false;
+		
+		try
+		{
+			if(needsMigration(database))
+			{
+				createTemporaryTable(database);
+				
+				database.updateQuery("DROP TABLE IF EXISTS Reports");
+				
+				ReporterDatabaseUtil.createTables(database);
+				
+				migrateTable(database);
+				
+				deleteTemporaryTable(database);
+				
+				updated = true;
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			Reporter.getLog().severe(Reporter.getDefaultConsolePrefix() + "An error occured while upgrading database data to version 9!");
+			Reporter.getLog().severe(Reporter.getDefaultConsolePrefix() + "If you receive more errors, you may have to delete your database!");
+		}
+		finally
+		{
+			try
+			{
+				database.closeConnection();
+			}
+			catch(Exception e)
+			{
+			}
+		}
+		
+		return updated;
+	}
+	
+	private static void deleteTemporaryTable(Database database) throws ClassNotFoundException, SQLException
+	{
+		database.updateQuery("DROP TABLE IF EXISTS Version9Temporary");
+	}
+
+	private static void createTemporaryTable(Database database) throws ClassNotFoundException, SQLException
+	{
+		String query = "CREATE TABLE IF NOT EXISTS Version9Temporary (" +
+				"ID INTEGER PRIMARY KEY, " +
+				"Date CHAR(19) NOT NULL DEFAULT 'N/A', " +
+				"SenderUUID CHAR(36) DEFAULT '', " +
+				"Sender VARCHAR(32), " +
+				"ReportedUUID CHAR(36) DEFAULT '', " +
+				"Reported VARCHAR(32) NOT NULL DEFAULT '* (Anonymous)', " +
+				"Details VARCHAR(200) NOT NULL, " +
+				"Priority TINYINT NOT NULL DEFAULT '0', " +
+				"SenderWorld VARCHAR(100) DEFAULT '', " +
+				"SenderX DOUBLE NOT NULL DEFAULT '0.0', " +
+				"SenderY DOUBLE NOT NULL DEFAULT '0.0', " +
+				"SenderZ DOUBLE NOT NULL DEFAULT '0.0', " +
+				"ReportedWorld VARCHAR(100) DEFAULT '', " +
+				"ReportedX DOUBLE DEFAULT '0.0', " +
+				"ReportedY DOUBLE DEFAULT '0.0', " +
+				"ReportedZ DOUBLE DEFAULT '0.0', " +
+				"CompletionStatus BOOLEAN NOT NULL DEFAULT '0', " +
+				"CompletedByUUID CHAR(36) DEFAULT '', " +
+				"CompletedBy VARCHAR(32) DEFAULT '', " +
+				"CompletionDate CHAR(19) DEFAULT '', " +
+				"CompletionSummary VARCHAR(200) DEFAULT '', " +
+				"ClaimStatus BOOLEAN NOT NULL DEFAULT '0', " +
+				"ClaimDate CHAR(19) DEFAULT '', " +
+				"ClaimedByUUID CHAR(36) DEFAULT '', " +
+				"ClaimedBy VARCHAR(32) DEFAULT '', " +
+				"ClaimPriority TINYINT DEFAULT '0');";
+		
+		database.updateQuery(query);
+		
+		database.updateQuery("INSERT INTO Version9Temporary " +
+				"SELECT * FROM Reports");
+	}
+
+	protected static boolean needsMigration(Database database) throws ClassNotFoundException, SQLException
+	{
+		if(database.checkTable("Reports"))
+		{
+			String columnName = "COLUMN_NAME";
+			String columnSize = "COLUMN_SIZE";
+			
+			SQLResultSet cols = new SQLResultSet();
+			ResultSet rs = null;
+			
+			try
+			{
+				rs = database.getColumnMetaData("Reports");
+				
+				cols.set(rs);
+			}
+			finally
+			{
+				try
+				{
+					rs.close();
+					database.closeConnection();
+				}
+				catch(SQLException e)
+				{
+				}
+			}
+			
+			if((cols.contains(columnName, "Sender") && cols.get(columnName, "Sender").getInt(columnSize) != 32)
+					&& (cols.contains(columnName, "Reported") && cols.get(columnName, "Reported").getInt(columnSize) != 32)
+					&& (cols.contains(columnName, "ClaimedBy") && cols.get(columnName, "ClaimedBy").getInt(columnSize) != 32)
+					&& (cols.contains(columnName, "CompletedBy") && cols.get(columnName, "CompletedBy").getInt(columnSize) != 32))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private static void migrateTable(Database database) throws ClassNotFoundException, SQLException
+	{
+		String query = "INSERT INTO Reports SELECT * FROM Version9Temporary";
+		
+		database.updateQuery(query);
+	}
+}
