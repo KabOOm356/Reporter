27358225e13ad45bde5765652acc8872f3f838b9
KabOOm 356 <kaboom356@gmail.com>
2016-04-10 22:27:26 -0500

    Reformatted, fixed errors and increased unit test coverage in the ArrayUtil class.

--- src/main/java/net/KabOOm356/Util/ArrayUtil.java
+++ src/main/java/net/KabOOm356/Util/ArrayUtil.java
@@ -6,221 +6,154 @@ import org.apache.commons.lang.Validate;
 import org.bukkit.ChatColor;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 /**
  * Utility class to handle arrays.
  */
-public final class ArrayUtil
-{
+public final class ArrayUtil {
 	public static final String defaultEntrySeparator = ", ";
 	public static final String defaultElementSeparator = "=";
-	
+
 	// Prevent instantiation.
-	private ArrayUtil()
-	{}
-	
+	private ArrayUtil() {
+	}
+
 	/**
 	 * Converts a Java array to an {@link ArrayList}.
-	 * 
+	 *
 	 * @param array The array to convert to an {@link ArrayList}.
-	 * 
 	 * @return An {@link ArrayList} containing all elements of the given array.
 	 */
-	public static <T> ArrayList<T> arrayToArrayList(T[] array)
-	{
-		if(array == null)
+	public static <T> ArrayList<T> arrayToArrayList(final T[] array) {
+		if (array == null) {
 			throw new IllegalArgumentException("Parameter 'array' cannot be null!");
-		
-		ArrayList<T> arrayList = new ArrayList<T>();
-		
-		for(T element : array)
-			arrayList.add(element);
-		
+		}
+
+		final ArrayList<T> arrayList = new ArrayList<T>();
+		Collections.addAll(arrayList, array);
+
 		return arrayList;
 	}
-	
+
 	/**
 	 * Converts an array to a Set.
-	 * 
+	 *
 	 * @param array The array to convert to a Set.
-	 * 
 	 * @return A {@link HashSet} containing the elements of the given array.
 	 */
-	public static <T> Set<T> arrayToSet(T[] array)
-	{
+	public static <T> Set<T> arrayToSet(final T[] array) {
 		Validate.notNull(array, "Parameter 'array' cannot be null!");
-		
-		Set<T> set = new HashSet<T>();
-		
-		for(T element : array)
-		{
-			set.add(element);
-		}
-		
+
+		final Set<T> set = new HashSet<T>();
+		Collections.addAll(set, array);
+
 		return set;
 	}
 
 	/**
 	 * Converts a map to a String.
 	 * The output format is: key1=value1, key2=value2
-	 * 
+	 *
 	 * @param map The map to convert to a String.
-	 * 
 	 * @return A String of all the elements of the given map.
 	 */
-	public static <T extends Map<K, V>, K, V> String indexesToString(T map)
-	{
+	public static <T extends Map<K, V>, K, V> String indexesToString(final T map) {
 		return indexesToString(map, defaultElementSeparator, defaultEntrySeparator);
 	}
-	
+
 	/**
 	 * Converts a map to a String.
 	 * The output format is: key1[elementSeparator]value1[entrySeparator]key2[elementSeparator]value2
-	 * 
-	 * @param map The map to convert to a String.
+	 *
+	 * @param map              The map to convert to a String.
 	 * @param elementSeparator The separator to be between the elements.
-	 * @param entrySeparator The separator to be between the entries.
-	 * 
+	 * @param entrySeparator   The separator to be between the entries.
 	 * @return A String of all elements of the given map.
 	 */
-	public static <T extends Map<K, V>, K, V> String indexesToString(final T map, final String elementSeparator, final String entrySeparator)
-	{
+	public static <T extends Map<K, V>, K, V> String indexesToString(final T map, final String elementSeparator, final String entrySeparator) {
 		Validate.notNull(map, "Parameter 'map' cannot be null!");
 		Validate.notNull(elementSeparator);
 		Validate.notNull(entrySeparator);
-		StringBuilder builder = new StringBuilder();
-		
-		for(Entry<K, V> e : map.entrySet())
-		{
+		final StringBuilder builder = new StringBuilder();
+
+		for (final Entry<K, V> e : map.entrySet()) {
 			builder.append(e.getKey().toString());
 			builder.append(elementSeparator);
 			builder.append(e.getValue().toString());
 			builder.append(entrySeparator);
 		}
-		
-		if(builder.length() > 2)
-		{
-			return builder.substring(0, builder.length() - 2);
+
+		if (builder.length() > entrySeparator.length() && builder.lastIndexOf(entrySeparator) != -1) {
+			return builder.substring(0, builder.lastIndexOf(entrySeparator));
 		}
-		
+
 		return builder.toString();
 	}
 
 	/**
 	 * Converts the given array into a string representation.
-	 * 
+	 *
 	 * @param array The array to format.
-	 * 
 	 * @return The string representation of the given indexes.
 	 */
-	public static <T extends Iterable<V>, V> String indexesToString(T array)
-	{
-		return indexesToString(array, defaultEntrySeparator);
+	public static <T extends Iterable<V>, V> String indexesToString(final T array) {
+		return indexesToString(array, "", defaultEntrySeparator);
 	}
-	
-	/**
-	 * Converts the given array into a String representation.
-	 * 
-	 * @param array The array to format.
-	 * @param entrySeparator The separator to be between each entry.
-	 * 
-	 * @return The String representation of the given array.
-	 */
-	public static <T extends Iterable<V>, V> String indexesToString(final T array, final String entrySeparator)
-	{
+
+	public static <V, T extends Iterable<V>> String indexesToString(final T array, final String indexPrefix, final String indexPostfix) {
 		Validate.notNull(array, "Parameter 'array' cannot be null!");
-		Validate.notNull(entrySeparator);
-		
-		StringBuilder builder = new StringBuilder();
-		
-		for(V index : array)
-		{
-			builder.append(index.toString()).append(entrySeparator);
+		Validate.notNull(indexPrefix);
+		Validate.notNull(indexPostfix);
+
+		final StringBuilder builder = new StringBuilder();
+
+		for (final V index : array) {
+			builder.append(indexPrefix).append(index.toString()).append(indexPostfix);
 		}
-		
-		if(builder.length() > 2)
-		{
-			return builder.substring(0, builder.length() - 2);
+
+		if (builder.length() > indexPostfix.length() && builder.lastIndexOf(indexPostfix) != -1) {
+			return builder.substring(0, builder.lastIndexOf(indexPostfix));
 		}
-		
+
 		return builder.toString();
 	}
 
 	/**
 	 * Converts the given array into a string representation.
-	 * 
-	 * @param array The array to format.
-	 * @param indexColor The color to make the indexes.
+	 *
+	 * @param array          The array to format.
+	 * @param indexColor     The color to make the indexes.
 	 * @param separatorColor The color to make the separator.
-	 * 
 	 * @return The string representation of the given array.
 	 */
-	public static <T extends Iterable<V>, V> String indexesToString(T array, ChatColor indexColor, ChatColor separatorColor)
-	{
-		if(array == null)
-			throw new IllegalArgumentException("Parameter 'array' cannot be null!");
-		if(indexColor == null)
-			throw new IllegalArgumentException("Parameter 'indexColor' cannot be null!");
-		if(separatorColor == null)
-			throw new IllegalArgumentException("Parameter 'separatorColor' cannot be null!");
-		
-		StringBuilder builder = new StringBuilder();
-		
-		for(V index : array)
-		{
-			builder.append(indexColor);
-			builder.append(index.toString());
-			builder.append(separatorColor);
-			builder.append(", ");
-		}
-		
-		if(builder.length() > 2)
-			return builder.substring(0, builder.length() - 2);
-		
-		return builder.toString();
+	public static <T extends Iterable<V>, V> String indexesToString(final T array, final ChatColor indexColor, final ChatColor separatorColor) {
+		Validate.notNull(indexColor);
+		Validate.notNull(separatorColor);
+		return indexesToString(array, indexColor.toString(), separatorColor + defaultEntrySeparator);
 	}
 
 	/**
 	 * Converts the given {@link SQLResultSet} of indexes to a string representation.
-	 * 
-	 * @param resultSet The indexes in {@link SQLResultSet} format.
-	 * @param columnName The name of the column in the {@link SQLResultSet}.
-	 * @param indexColor The color to make the indexes.
+	 *
+	 * @param resultSet      The indexes in {@link SQLResultSet} format.
+	 * @param columnName     The name of the column in the {@link SQLResultSet}.
+	 * @param indexColor     The color to make the indexes.
 	 * @param separatorColor The color to make the separator.
-	 * 
 	 * @return The string representation of the given indexes.
-	 * 
 	 * @throws SQLException
 	 */
-	public static String indexesToString(SQLResultSet resultSet, String columnName, ChatColor indexColor, ChatColor separatorColor)
-	{
-		if(resultSet == null)
-			throw new IllegalArgumentException("Parameter 'resultSet' cannot be null!");
-		if(columnName == null)
-			throw new IllegalArgumentException("Parameter 'columnName' cannot be null!");
-		if(indexColor == null)
-			throw new IllegalArgumentException("Parameter 'indexColor' cannot be null!");
-		if(separatorColor == null)
-			throw new IllegalArgumentException("Parameter 'separatorColor' cannot be null!");
-		
-		StringBuilder indexString = new StringBuilder();
-		
-		for(ResultRow row : resultSet)
-		{
-			indexString.append(indexColor);
-			indexString.append(row.getString(columnName));
-			indexString.append(separatorColor);
-			indexString.append(", ");
+	public static String indexesToString(final SQLResultSet resultSet, final String columnName, final ChatColor indexColor, final ChatColor separatorColor) {
+		Validate.notNull(resultSet);
+		Validate.notNull(columnName);
+		Validate.notNull(indexColor);
+		Validate.notNull(separatorColor);
+
+		final ArrayList<String> array = new ArrayList<String>();
+		for (final ResultRow row : resultSet) {
+			array.add(row.getString(columnName));
 		}
-		
-		if(indexString.length() > 2)
-			return indexString.substring(0, indexString.length() - 2);
-		
-		return indexString.toString();
+		return indexesToString(array, indexColor.toString(), separatorColor + defaultEntrySeparator);
 	}
 }
--- src/test/java/test/KabOOm356/Util/ArrayUtilTest.java
+++ src/test/java/test/KabOOm356/Util/ArrayUtilTest.java
@@ -1,39 +1,38 @@
 package test.KabOOm356.Util;
 
+import net.KabOOm356.Database.ResultRow;
+import net.KabOOm356.Database.SQLResultSet;
 import net.KabOOm356.Util.ArrayUtil;
+import org.bukkit.ChatColor;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.sql.SQLException;
+import java.util.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
-public class ArrayUtilTest
-{
+public class ArrayUtilTest {
 	@Test(expected = IllegalArgumentException.class)
-	public void testArrayToArrayListNull()
-	{
+	public void testArrayToArrayListNull() {
 		ArrayUtil.arrayToArrayList(null);
 	}
-	
+
 	@Test
-	public void testArrayToArrayList()
-	{
+	public void testArrayToArrayList() {
 		Integer[] emptyIntArray = new Integer[0];
 		ArrayList<Integer> intList = new ArrayList<Integer>();
-		
+
 		assertEquals(intList, ArrayUtil.arrayToArrayList(emptyIntArray));
-		
+
 		Integer[] oneIntArray = {2};
 		intList.add(2);
 		assertEquals(intList, ArrayUtil.arrayToArrayList(oneIntArray));
-		
+
 		intList.clear();
 		intList.add(5);
 		assertNotEquals(intList, ArrayUtil.arrayToArrayList(oneIntArray));
-		
-		Integer[] intArray = {1,2,3,4,5};
+
+		Integer[] intArray = {1, 2, 3, 4, 5};
 		intList.clear();
 		intList.add(1);
 		intList.add(2);
@@ -41,7 +40,7 @@ public class ArrayUtilTest
 		intList.add(4);
 		intList.add(5);
 		assertEquals(intList, ArrayUtil.arrayToArrayList(intArray));
-		
+
 		intList.clear();
 		intList.add(3);
 		intList.add(2);
@@ -49,7 +48,7 @@ public class ArrayUtilTest
 		intList.add(5);
 		intList.add(4);
 		assertNotEquals(intList, ArrayUtil.arrayToArrayList(intArray));
-		
+
 		intArray[3] = 2;
 		intList.clear();
 		intList.add(1);
@@ -59,30 +58,28 @@ public class ArrayUtilTest
 		intList.add(5);
 		assertEquals(intList, ArrayUtil.arrayToArrayList(intArray));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void testArrayToSetNull()
-	{
+	public void testArrayToSetNull() {
 		ArrayUtil.arrayToSet(null);
 	}
-	
+
 	@Test
-	public void testArrayToSet()
-	{
+	public void testArrayToSet() {
 		Integer[] emptyIntArray = new Integer[0];
 		HashSet<Integer> intSet = new HashSet<Integer>();
-		
+
 		assertEquals(intSet, ArrayUtil.arrayToSet(emptyIntArray));
-		
+
 		Integer[] oneIntArray = {2};
 		intSet.add(2);
 		assertEquals(intSet, ArrayUtil.arrayToSet(oneIntArray));
-		
+
 		intSet.clear();
 		intSet.add(5);
 		assertNotEquals(intSet, ArrayUtil.arrayToSet(oneIntArray));
-		
-		Integer[] intArray = {1,2,3,4,5};
+
+		Integer[] intArray = {1, 2, 3, 4, 5};
 		intSet.clear();
 		intSet.add(1);
 		intSet.add(2);
@@ -90,7 +87,7 @@ public class ArrayUtilTest
 		intSet.add(4);
 		intSet.add(5);
 		assertEquals(intSet, ArrayUtil.arrayToSet(intArray));
-		
+
 		intSet.clear();
 		intSet.add(3);
 		intSet.add(2);
@@ -98,7 +95,7 @@ public class ArrayUtilTest
 		intSet.add(5);
 		intSet.add(4);
 		assertEquals(intSet, ArrayUtil.arrayToSet(intArray));
-		
+
 		intArray[3] = 2;
 		intSet.clear();
 		intSet.add(1);
@@ -107,7 +104,7 @@ public class ArrayUtilTest
 		intSet.add(2);
 		intSet.add(5);
 		assertEquals(intSet, ArrayUtil.arrayToSet(intArray));
-		
+
 		intArray[0] = 2;
 		intArray[1] = 2;
 		intArray[2] = 4;
@@ -118,31 +115,115 @@ public class ArrayUtilTest
 		intSet.add(4);
 		assertEquals(intSet, ArrayUtil.arrayToSet(intArray));
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testIndexesToStringNull() {
-		ArrayUtil.indexesToString((ArrayList<String>)null);
+		ArrayUtil.indexesToString((ArrayList<String>) null);
 	}
-	
+
 	@Test
-	public void testIndexesToString()
-	{
+	public void testIndexesToString() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		
+
 		String value = ArrayUtil.indexesToString(list);
 		assertEquals("", value);
-		
+
 		list.add(1);
-		
+
 		value = ArrayUtil.indexesToString(list);
 		assertEquals("1", value);
-		
-		for(int LCV = 2; LCV < 10; LCV++)
-		{
+
+		for (int LCV = 2; LCV < 10; LCV++) {
 			list.add(LCV);
 		}
-		
+
 		value = ArrayUtil.indexesToString(list);
 		assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9", value);
+	}
+
+	@Test
+	public void testIndexesToStringMapNull() {
+		try {
+			ArrayUtil.indexesToString((Map) null);
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+
+		try {
+			ArrayUtil.indexesToString((Map) null, "", "");
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+
+		try {
+			ArrayUtil.indexesToString(new HashMap(), null, "");
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+
+		try {
+			ArrayUtil.indexesToString(new HashMap(), "", null);
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+	}
+
+	@Test
+	public void testIndexesToStringMap() {
+		final Map<String, String> testMap = new LinkedHashMap<String, String>(16, 0.75f, true);
+		testMap.put("FirstKey", "FirstValue");
+		testMap.put("SecondKey", "SecondValue");
+		testMap.put("LastKey", "LastValue");
+
+		String returned = ArrayUtil.indexesToString(new HashMap());
+		assertEquals("", returned);
+
+		returned = ArrayUtil.indexesToString(testMap);
+		assertEquals("FirstKey=FirstValue, SecondKey=SecondValue, LastKey=LastValue", returned);
+
+		returned = ArrayUtil.indexesToString(testMap, ":", " ");
+		assertEquals("FirstKey:FirstValue SecondKey:SecondValue LastKey:LastValue", returned);
+	}
+
+	@Test
+	public void testIndexToStringChatColor() {
+		final ArrayList<String> testArray = new ArrayList<String>();
+		testArray.add("FirstElement");
+		testArray.add("SecondElement");
+		testArray.add("LastElement");
+
+		String returned = ArrayUtil.indexesToString(new ArrayList(), ChatColor.RED, ChatColor.WHITE);
+		String expected = "";
+		assertEquals(expected, returned);
+
+		returned = ArrayUtil.indexesToString(testArray, ChatColor.RED, ChatColor.WHITE);
+		expected = ChatColor.RED + "FirstElement" + ChatColor.WHITE + ", " + ChatColor.RED + "SecondElement" + ChatColor.WHITE + ", " + ChatColor.RED + "LastElement";
+		assertEquals(expected, returned);
+	}
+
+	@Test
+	public void testIndexesToStringSQLResult() throws SQLException {
+		final SQLResultSet resultSet = new SQLResultSet();
+		ResultRow resultRow = new ResultRow();
+		resultRow.put("TestColumn", 1);
+		resultSet.add(resultRow);
+		resultRow = new ResultRow();
+		resultRow.put("TestColumn", 3);
+		resultSet.add(resultRow);
+		resultRow = new ResultRow();
+		resultRow.put("TestColumn", "LastValue");
+		resultSet.add(resultRow);
+
+		String returned = ArrayUtil.indexesToString(new SQLResultSet(), "TestColumn", ChatColor.RED, ChatColor.WHITE);
+		String expected = "";
+		assertEquals(expected, returned);
+
+		returned = ArrayUtil.indexesToString(resultSet, "TestColumn", ChatColor.RED, ChatColor.WHITE);
+		expected = ChatColor.RED + "1" + ChatColor.WHITE + ", " + ChatColor.RED + "3" + ChatColor.WHITE + ", " + ChatColor.RED + "LastValue";
+		assertEquals(expected, returned);
 	}
 }
