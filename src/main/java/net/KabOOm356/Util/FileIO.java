0a1b6c0cbecb14644cc2dd5f09064eaf45948710
KabOOm 356 <kaboom356@gmail.com>
2016-04-13 22:01:00 -0500

    Reformatted and increased unit test coverage in the FileIO class.

--- src/main/java/net/KabOOm356/Util/FileIO.java
+++ src/main/java/net/KabOOm356/Util/FileIO.java
@@ -1,144 +1,124 @@
 package net.KabOOm356.Util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import net.KabOOm356.File.RevisionFile;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import net.KabOOm356.File.RevisionFile;
+import java.io.*;
 
 /**
  * A class to help with file input and output.
  */
-public class FileIO
-{
-	private static final Logger log = LogManager.getLogger(FileIO.class);
-	
+public class FileIO {
 	/**
 	 * The encoding to always use when writing to a file.
 	 */
 	public static final String encoding = "UTF-8";
-	
 	public static final String BACKUP_FILE_EXTENSION = ".backup";
-	
+	private static final Logger log = LogManager.getLogger(FileIO.class);
+
 	/**
 	 * Copies a given text {@link File} to another {@link File}.
 	 * <br /><br />
 	 * Input file is assumed to be encoded in UTF-8.
 	 * <br />
 	 * Output file is written in UTF-8
-	 * 
-	 * @param in The {@link File} to copy.
+	 *
+	 * @param in  The {@link File} to copy.
 	 * @param out The {@link File} to copy to.
-	 * 
 	 * @throws IOException
 	 * @throws IllegalArgumentException If either the file to read from or the file to write to are null.
-	 * 
 	 * @see FileIO#copyTextFile(File, File, String, String)
 	 */
-	public static void copyTextFile(File in, File out) throws IOException
-	{
+	public static void copyTextFile(final File in, final File out) throws IOException {
 		copyTextFile(in, out, encoding, encoding);
 	}
-	
+
 	/**
 	 * Copies a given text {@link File} to another {@link File}.
-	 * 
-	 * @param in The {@link File} to copy.
-	 * @param out The {@link File} to copy to.
-	 * @param inEncoding The encoding of the file to be copied.
+	 *
+	 * @param in          The {@link File} to copy.
+	 * @param out         The {@link File} to copy to.
+	 * @param inEncoding  The encoding of the file to be copied.
 	 * @param outEncoding The encoding to write the file to.
-	 * 
 	 * @throws IOException
 	 * @throws IllegalArgumentException If either the file to read from or the file to write to are null.
 	 */
-	public static void copyTextFile(File in, File out, String inEncoding, String outEncoding) throws IOException
-	{
-		if(in == null)
+	public static void copyTextFile(final File in, final File out, final String inEncoding, final String outEncoding) throws IOException {
+		if (in == null) {
 			throw new IllegalArgumentException("Input file cannot be null!");
-		if(out == null)
+		}
+		if (out == null) {
 			throw new IllegalArgumentException("Output file cannot be null!");
-		
-		if(!out.exists())
+		}
+
+		if (!out.exists()) {
 			out.createNewFile();
-		
-		if(!in.exists())
+		}
+
+		if (!in.exists()) {
 			in.createNewFile();
-		else
-		{
-			BufferedReader input = null;
-			BufferedWriter output = null;
-			String line;
-			
-			try
-			{
-				input = new BufferedReader(new InputStreamReader(in.toURI().toURL().openStream(), inEncoding));
-				output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out), outEncoding));
-				
-				while((line = input.readLine()) != null)
-				{
-					output.write(line);
-					output.newLine();
-				}
-				
-				output.flush();
+		}
+
+		BufferedReader input = null;
+		BufferedWriter output = null;
+		String line;
+
+		try {
+			input = new BufferedReader(new InputStreamReader(in.toURI().toURL().openStream(), inEncoding));
+			output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out), outEncoding));
+
+			while ((line = input.readLine()) != null) {
+				output.write(line);
+				output.newLine();
 			}
-			finally
-			{
-				try
-				{
+
+			output.flush();
+		} finally {
+			try {
+				if (input != null) {
 					input.close();
 				}
-				catch(Exception e)
-				{
+			} catch (final IOException e) {
+				if (log.isDebugEnabled()) {
+					log.warn("Closing input failed!", e);
 				}
-				
-				try
-				{
+			}
+
+			try {
+				if (output != null) {
 					output.close();
 				}
-				catch(Exception e)
-				{
+			} catch (final IOException e) {
+				if (log.isDebugEnabled()) {
+					log.warn("Closing output failed!", e);
 				}
 			}
 		}
 	}
-	
+
 	/**
 	 * Creates a backup.
-	 * 
+	 *
 	 * @param file The file to backup.
-	 * 
 	 * @return A {@link RevisionFile} where the given File was saved.
 	 */
-	public static RevisionFile createBackup(File file)
-	{
+	public static RevisionFile createBackup(final File file) {
 		RevisionFile backup = null;
-		
-		try
-		{
+
+		try {
 			backup = new RevisionFile(file.getParent(), file.getName() + BACKUP_FILE_EXTENSION);
-			
 			backup.incrementToNextRevision();
-			
 			backup.createNewFile();
-			
 			FileIO.copyTextFile(file, backup.getFile());
-		}
-		catch(final Exception e)
-		{
-			log.error("Failed to create backup file for " + file.getName() , e);
-			if(backup != null)
+		} catch (final Exception e) {
+			log.error("Failed to create backup file for " + file.getName(), e);
+			if (backup != null) {
 				backup.delete();
+			}
 			return null;
 		}
-		
+
 		return backup;
 	}
 }
---
+++ src/test/java/test/KabOOm356/Util/FileIOTest.java
@@ -0,0 +1,112 @@
+package test.KabOOm356.Util;
+
+import net.KabOOm356.File.RevisionFile;
+import net.KabOOm356.Util.FileIO;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import test.KabOOm356.PowerMockitoTest;
+
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@PrepareForTest({FileIO.class})
+public class FileIOTest extends PowerMockitoTest {
+	@Mock
+	public File file;
+
+	@Mock
+	public RevisionFile revisionFile;
+
+	@Mock
+	public URL url;
+
+	@Mock
+	public URI uri;
+
+	@Mock
+	public InputStream inputStream;
+
+	@Mock
+	public InputStreamReader inputStreamReader;
+
+	@Mock
+	public BufferedReader bufferedReader;
+
+	@Mock
+	public FileOutputStream fileOutputStream;
+
+	@Mock
+	public OutputStreamWriter outputStreamWriter;
+
+	@Mock
+	public BufferedWriter bufferedWriter;
+
+	@Before
+	public void setupMocks() throws Exception {
+		whenNew(BufferedReader.class).withAnyArguments().thenReturn(bufferedReader);
+		whenNew(InputStreamReader.class).withAnyArguments().thenReturn(inputStreamReader);
+		when(file.toURI()).thenReturn(uri);
+		when(uri.toURL()).thenReturn(url);
+		when(url.openStream()).thenReturn(inputStream);
+
+		whenNew(BufferedWriter.class).withAnyArguments().thenReturn(bufferedWriter);
+		whenNew(OutputStreamWriter.class).withAnyArguments().thenReturn(outputStreamWriter);
+		whenNew(FileOutputStream.class).withAnyArguments().thenReturn(fileOutputStream);
+
+		when(bufferedReader.readLine()).thenReturn("test", "string", null);
+
+		whenNew(RevisionFile.class).withAnyArguments().thenReturn(revisionFile);
+	}
+
+	@Test
+	public void testCopyTextFileNull() throws IOException {
+		try {
+			FileIO.copyTextFile(null, null);
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+
+		try {
+			FileIO.copyTextFile(file, null);
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+
+		try {
+			FileIO.copyTextFile(null, file);
+			fail("Expected exception not thrown!");
+		} catch (final IllegalArgumentException e) {
+			// Expected exception
+		}
+	}
+
+	@Test
+	public void testCopyTextFile() throws IOException {
+		FileIO.copyTextFile(file, file);
+		verify(bufferedWriter, times(2)).write(anyString());
+		verify(bufferedWriter).flush();
+		verify(bufferedWriter).close();
+		verify(bufferedReader).close();
+	}
+
+	@Test
+	public void testCreateBackup() throws IOException {
+		mockStatic(FileIO.class);
+		when(FileIO.createBackup(file)).thenCallRealMethod();
+		assertEquals(revisionFile, FileIO.createBackup(file));
+		verify(revisionFile).incrementToNextRevision();
+		verify(revisionFile).createNewFile();
+	}
+}
