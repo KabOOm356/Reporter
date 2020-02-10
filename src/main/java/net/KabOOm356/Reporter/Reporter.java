0cdd9c71326f4e77ad70982ec8e1f30fbe9095a5
Robert Wagner <kaboom356@gmail.com>
2020-02-10 17:01:48 -0600

    Version 3.3.3.

--- src/main/java/net/KabOOm356/Reporter/Reporter.java
+++ src/main/java/net/KabOOm356/Reporter/Reporter.java
@@ -53,7 +53,7 @@ import java.util.concurrent.Callable;
  */
 @Plugin(
 		name = "Reporter",
-		version = "3.3.3 BETA"
+		version = "3.3.3"
 )
 @ApiVersion(ApiVersion.Target.v1_13)
 @Author("KabOOm 356")
